4cf352df06afca871d201a507f6b700c8106620f
bradselcuk <bradselcuk@gmail.com>
2021-07-15 14:30:00 -0500

    Initial commit

---
+++ Sample2.py
@@ -0,0 +1,7 @@
+for sayi in range(1, 20, 1):
+    cevap = int(input("Parola giriniz."))
+    if cevap == 148:
+        print("Girdiğiniz parola doğru")
+        break
+    else:
+        print("Girdiğiniz parola yanlış")
